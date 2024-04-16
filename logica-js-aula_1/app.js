d3ab3a8f8513acf393c925d2cc21e0db98ba3289
Bárbara Ayres <ayresbarbara2@gmail.com>
2024-04-16 18:01:13 -0300

    Concatenação

--- logica-js-aula_1/app.js
+++ logica-js-aula_1/app.js
@@ -1,10 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    alert("Isso ai! Você descobriu o número secreto (5)");
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 
 } else{
     alert("Você errou :(")
